b9c763c06e21fd0aad85260285b08e28a694c059
Ashley Glime <>
2018-11-24 15:32:20 -0500

    Updates

---
+++ precache-manifest.e59629e9abb6dd43709d1c6a0d58638f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "20029764d016fe1f5394",
+    "url": "/react-tutorial-taniarascia/static/js/runtime~main.20029764.js"
+  },
+  {
+    "revision": "0d0547ecc39a45c67366",
+    "url": "/react-tutorial-taniarascia/static/js/main.0d0547ec.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-tutorial-taniarascia/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "0d0547ecc39a45c67366",
+    "url": "/react-tutorial-taniarascia/static/css/main.36fdd9b3.chunk.css"
+  },
+  {
+    "revision": "d988df19c6ac106a01289412a64775cb",
+    "url": "/react-tutorial-taniarascia/index.html"
+  }
+];
